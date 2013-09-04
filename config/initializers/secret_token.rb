49191629b38cb48fa6011bf4234da9b67fc7e912
Cody Tanner <cerebralstorm@gmail.com>
2013-09-04 13:17:12 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fortis::Application.config.secret_key_base = '0bb46632fbed70ef15bda09d36200f1e5c76ac3c812f11641b19d8727b7f00aaf8d5a3c3b2c0bbe1b6b74110501d44322f66236e8c9303b1b4d11a88d80edb61'
