d080b8b9cb64fda5eb82e052e8e9bb2fbfa8c493
Cody Tanner <cerebralstorm@gmail.com>
2013-09-06 19:30:15 -0600

    adds workout validations and tests and fixes workout routes

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,5 +2,8 @@ class Workout < ActiveRecord::Base
   belongs_to :user
   has_many :activities
 
+  validates_presence_of :user_id
+  validates_presence_of :date
+
   accepts_nested_attributes_for :activities, allow_destroy: true
 end
