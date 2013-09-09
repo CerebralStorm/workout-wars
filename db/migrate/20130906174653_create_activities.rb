a689c6c4457d8da12b508095f4d04f7ca53944d8
Cody Tanner <cerebralstorm@gmail.com>
2013-09-08 22:56:27 -0600

    Adds some workout silhouette vector graphics and adds flag fields to activities

--- db/migrate/20130906174653_create_activities.rb
+++ db/migrate/20130906174653_create_activities.rb
@@ -8,6 +8,14 @@ class CreateActivities < ActiveRecord::Migration
       t.integer :sets
       t.float :duration
       t.float :distance
+      t.integer :calories
+      t.integer :heart_rate
+      t.boolean :use_reps, default: false
+      t.boolean :use_sets, default: false
+      t.boolean :use_duration, default: false
+      t.boolean :use_distance, default: false
+      t.boolean :use_calories, default: false
+      t.boolean :use_heart_rate, default: false
 
       t.timestamps
     end
