8f3594e2480d5fbec4e9dbb7a9452a2564238fd5
Cody Tanner <cerebralstorm@gmail.com>
2013-09-09 22:54:22 -0600

    refactors workout form

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,6 +3,12 @@ class Exercise < ActiveRecord::Base
   belongs_to :activity
 
   delegate :name, to: :activity
+  delegate :use_reps, to: :activity
+  delegate :use_sets, to: :activity
+  delegate :use_duration, to: :activity
+  delegate :use_distance, to: :activity
+  delegate :use_weight, to: :activity
+  delegate :fields, to: :activity
   validates_presence_of :activity_id
 
 end
