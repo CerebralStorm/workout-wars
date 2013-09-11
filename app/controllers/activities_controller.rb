2f3caf9e1e27cfc690f236a019c51a6d8cbb1889
Cody Tanner <cerebralstorm@gmail.com>
2013-09-11 10:33:57 -0600

    adds the rest of the activity files.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,24 +1,35 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
+  # GET /activities
+  # GET /activities.json
   def index
-    Activity.all
+    @activities = Activity.all
   end
 
+  # GET /activities/1
+  # GET /activities/1.json
   def show
-    Activity.find(params[:id])
   end
 
+  # GET /activities/new
+  def new
+    @activity = Activity.new
+  end
+
+  # GET /activities/1/edit
   def edit
   end
 
+  # POST /activities
+  # POST /activities.json
   def create
-    @activity = Activity.create(activity_params)
+    @activity = Activity.new(activity_params)
 
     respond_to do |format|
       if @activity.save
-        format.html { redirect_to activity_path(@activity), notice: 'Activity was successfully created.' }
-        format.json { render action: 'show', status: :created, location: activity_path(@activity) }
+        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @activity }
       else
         format.html { render action: 'new' }
         format.json { render json: @activity.errors, status: :unprocessable_entity }
@@ -26,10 +37,12 @@ class ActivitiesController < ApplicationController
     end
   end
 
+  # PATCH/PUT /activities/1
+  # PATCH/PUT /activities/1.json
   def update
     respond_to do |format|
       if @activity.update(activity_params)
-        format.html { redirect_to activity_path(@activity), notice: 'Activity was successfully updated.' }
+        format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -38,22 +51,24 @@ class ActivitiesController < ApplicationController
     end
   end
 
+  # DELETE /activities/1
+  # DELETE /activities/1.json
   def destroy
     @activity.destroy
     respond_to do |format|
-      format.html { redirect_to activities_path }
+      format.html { redirect_to activities_url }
       format.json { head :no_content }
     end
   end
 
-private
-  
-  def set_activity
-    @activity = Workout.find(params[:id])
-  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_activity
+      @activity = Activity.find(params[:id])
+    end
 
-  def activity_params
-    params.require(:activity).permit(:name, :use_reps, :use_sets, :use_duration, :use_distance, :suse_weight)
-  end
-
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def activity_params
+      params[:activity]
+    end
 end
