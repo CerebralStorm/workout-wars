b543daf19439a45e32c2ec155b3a263bb63cde12
Cody Tanner <cerebralstorm@gmail.com>
2013-09-10 20:58:58 -0600

    fixes competitions

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,12 +1,14 @@
 class Competition < ActiveRecord::Base
   has_many :users, through: :competition_subscriptions
   has_many :competition_subscriptions
-  has_one :difficulty
+  belongs_to :difficulty
+
+  delegate :level, to: :difficulty
 
   validates_presence_of :name
   validates_presence_of :start_date
   validates_presence_of :end_date
-  validates_presence_of :difficulty
+  validates_presence_of :difficulty_id
   validates_presence_of :creator_id
   
 end
