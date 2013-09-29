8a2d515c7655ece917686108ecde065b4fc389ee
Cody Tanner <cerebralstorm@gmail.com>
2013-09-29 09:32:19 -0600

    adds back remove_xp for exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,8 +1,10 @@
 class Exercise < ActiveRecord::Base
   belongs_to :user
   belongs_to :exercise_type
-  has_many :xp_transactions, dependent: :destroy
+  has_many :xp_transactions
+  has_many :competition_transactions, dependent: :destroy
   after_save :update_user
+  after_destroy :remove_xp
 
   delegate :category, to: :exercise_type
   delegate :name, to: :exercise_type
@@ -36,5 +38,9 @@ class Exercise < ActiveRecord::Base
       XpTransaction.create(amount: total_xp, user_id: user.id, xp_source_type: "exercise", xp_source_id: self.id)
     end
     user.set_level
+  end
+
+  def remove_xp
+    XpTransaction.find_by user_id: user.id , xp_source_type: "exercise", xp_source_id: self.id
   end
 end
